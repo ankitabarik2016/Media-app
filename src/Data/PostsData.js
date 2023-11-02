c4b16e99c5f2520153be7d196d11d1d59c8a477b
ankitabarik2016 <ankitabarik2016@gmail.com>
2023-11-02 17:03:45 +0530

    first commit

---
+++ src/Data/PostsData.js
@@ -0,0 +1,27 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic1.jpg'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: "Tzuyu",
+        desc: "Happy New Year! #2024",
+        likes: 4900,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: "Marina",
+        desc: "Party Time :)",
+        likes: 1040,
+        liked: false
+    },
+    {
+        img: postPic3,
+        name: "Tzuyu",
+        desc: "Happy New Year! #2024",
+        likes: 4900,
+        liked: true
+    }
+]
---
+++ src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData =[
+    {
+        name:"Minions",
+        Shares:91,
+    },
+    {
+        name:"Avengers",
+        Shares:92,
+    },
+    {
+        name:"Zara",
+        Shares:93,
+    },
+    {
+        name:"Reactjs",
+        Shares:94,
+    },
+    {
+        name:"Elon Musk",
+        Shares:95,
+    },
+    {
+        name:"Need for Anything",
+        Shares:96,
+    },
+];
